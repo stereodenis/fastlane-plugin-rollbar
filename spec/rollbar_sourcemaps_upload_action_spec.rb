77d52190dc8db765db10d583f1c1b91c36437ce6
Evgrafov Denis <stereodenis@gmail.com>
2020-07-09 13:54:19 +0300

    sample spec

--- spec/rollbar_sourcemaps_upload_action_spec.rb
+++ spec/rollbar_sourcemaps_upload_action_spec.rb
@@ -1,11 +1,17 @@
-# frozen_string_literal: true
+require 'spec_helper'
 
 describe Fastlane::Actions::RollbarSourcemapsUploadAction do
   describe '#run' do
     it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with('The rollbar plugin is working!')
+      expect(Fastlane::UI).to receive(:message).with('Creating React Native bundle')
+      expect(Fastlane::UI).to receive(:message).with('Uploading React Native bundle to Rollbar')
 
-      Fastlane::Actions::RollbarSourcemapsUploadAction.run(nil)
+      Fastlane::Actions::RollbarSourcemapsUploadAction.run(
+        os: 'android',
+        api_key: 'abc',
+        code_version: '1.0.0',
+        environment: 'production'
+      )
     end
   end
 end
